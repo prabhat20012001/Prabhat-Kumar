fdc752bac189ae715161af11b3358ee3772ba3bc
prabhat20012001 <prabhatkumarmnnit@gmail.com>
2024-02-27 11:32:33 +0530

    Header design completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className='container home'>
         <div className="left">
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, error.</h1>
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
             <a href={pdf} download="Prabhat-resume.pdf" className="btn btn-outline-warning">Download Resume</a>
         </div>
         <div className="right">
